c4d34c8083763cebde704a51daad1621e4393337
devuri <devuriko@gmail.com>
2021-02-06 12:22:02 -0500

    minor code style fix

--- tests/OembedDataTest.php
+++ tests/OembedDataTest.php
@@ -9,32 +9,32 @@ class OembedHttpDataTest extends TestCase
 {
 
 	/** @var string */
-    protected $url = 'https://www.youtube.com/watch?v=wfN4PVaOU5Q';
+	protected $url = 'https://www.youtube.com/watch?v=wfN4PVaOU5Q';
 
 	/** @var string */
-    protected $invalid_url = 'httube.com/watch?v=wfN4PVaOU5Q';
+	protected $invalid_url = 'httube.com/watch?v=wfN4PVaOU5Q';
 
-    protected function setUp(): void
-    {
-        parent::setUp();
+	protected function setUp(): void
+	{
+		parent::setUp();
 
-    }
+	}
 
 	/** @test */
 	public function can_get_video_id(): void
-    {
+	{
 		$youtube = (new YouTube());
-        $this->assertEquals(
-            'wfN4PVaOU5Q',
-            $youtube->id( $this->url )
-        );
-    }
+		$this->assertEquals(
+			'wfN4PVaOU5Q',
+			$youtube->id( $this->url )
+		);
+	}
 
 	/** @test */
 	public function throws_exception_for_invalid_url()
-    {
+	{
 		$youtube = (new YouTube());
-        $this->expectException( InvalidArgumentException::class );
-        $youtube->id( $this->invalid_url );
-    }
+		$this->expectException( InvalidArgumentException::class );
+		$youtube->id( $this->invalid_url );
+	}
 }
